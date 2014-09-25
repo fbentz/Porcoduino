db31b711d50211c474b2acdae177cf4e49c119b3
Robin Passama <passama@lirmm.fr>
2014-09-25 22:34:41 +0200

    refactored again

--- src/firmware/Zeppelin/Zeppelin.ino
+++ src/firmware/Zeppelin/Zeppelin.ino
@@ -1,41 +1,51 @@
+//general definitions to interface with the hardware 
 
+
+#define UP_MOTOR_FORWARD 5
+#define UP_MOTOR_BACKWARD 6
+#define UP_MOTOR_ENABLE 10
+
+#define RIGHT_MOTOR_FORWARD 7
+#define RIGHT_MOTOR_BACKWARD 8
+#define RIGHT_MOTOR_ENABLE 3
+
+#define LEFT_MOTOR_FORWARD 13
+#define LEFT_MOTOR_BACKWARD 12
+#define LEFT_MOTOR_ENABLE 11
+
+//general definitions as parameters of the  
 #define MOTOR_MAX_PWM 255.0
 
-const int motor_right_forward = 7;
-const int motor_right_backward = 8;
-const int motor_right_enable = 3;
-
-const int motor_left_forward = 13;
-const int motor_left_backward = 12;
-const int motor_left_enable = 11;
-
-const int motor_up_forward = 5;//TODO set the adequate value
-const int motor_up_backward = 6;//TODO set the adequate value
-const int motor_up_enable = 10;//TODO set the adequate value
 
 
-byte motor_right_pwm = 0;
-byte motor_left_pwm = 0;
-byte motor_up_pwm = 0;
 
-float motor_right_speed = 0;
-float motor_left_speed = 0;
-float motor_up_speed = 0;
+byte right_motor_pwm = 0;
+byte left_motor_pwm = 0;
+
+float right_motor_velocity = 0;
+float left_motor_velocity = 0;
 
 char incomingByte_X = 0;
 char incomingByte_Y = 0;
-char incomingByte_Z = 0;
 
 const float X_factor = 1.0;
 const float Y_factor = 1.0;
-const float Z_factor = 1.0;
 
 float min_right;
 float max_right;
 float min_left;
 float max_left;
+
+
+#ifndef TEST_XY_ONLY
+byte up_motor_pwm = 0;
+float up_motor_velocity = 0;
+char incomingByte_Z = 0;
+const float Z_factor = 1.0;
 float min_up;
 float max_up;
+#endif
+
 
 const long interval = 200;     
 long previousMillis = 0;
@@ -47,40 +57,45 @@ float sign(char number){
 
 
 //computing Motor velocity VS an X,Y control vector
-float compute_Right_Motor_Raw_Speed(float value_x, float value_y){
+float compute_Right_Motor_Raw_Velocity(float value_x, float value_y){
   return (X_factor*value_x - Y_factor*value_y*sign(value_x));
 }
 
-float compute_Left_Motor_Raw_Speed(float value_x, float value_y){
+float compute_Left_Motor_Raw_Velocity(float value_x, float value_y){
   return (X_factor*value_x + Y_factor*value_y*sign(value_x));
 }
 
-float compute_Up_Motor_Raw_Speed(float value_z){
+#ifndef TEST_XY_ONLY
+float compute_Up_Motor_Raw_Velocity(float value_z){
   return (Z_factor*value_z);
 }
+#endif
 
 //computing Motor velocity VS an X,Y control vector
-float compute_Right_Motor_Speed(float value_x, float value_y){
-    return map(compute_Right_Motor_Raw_Speed(value_x, value_y), min_right, max_right,-MOTOR_MAX_PWM,MOTOR_MAX_PWM);
+float compute_Right_Motor_Velocity(float value_x, float value_y){
+    return map(compute_Right_Motor_Raw_Velocity(value_x, value_y), min_right, max_right,-MOTOR_MAX_PWM,MOTOR_MAX_PWM);
 }
 
-float compute_Left_Motor_Speed(float value_x, float value_y){
-  return map(compute_Left_Motor_Raw_Speed(value_x, value_y), min_left, max_left,-MOTOR_MAX_PWM,MOTOR_MAX_PWM);
+float compute_Left_Motor_Velocity(float value_x, float value_y){
+  return map(compute_Left_Motor_Raw_Velocity(value_x, value_y), min_left, max_left,-MOTOR_MAX_PWM,MOTOR_MAX_PWM);
 }
 
-float compute_Up_Motor_Speed(float value_z){
-  return map(compute_Up_Motor_Raw_Speed(value_z), min_up, max_up,-MOTOR_MAX_PWM,MOTOR_MAX_PWM);
+#ifndef TEST_XY_ONLY
+float compute_Up_Motor_Velocity(float value_z){
+  return map(compute_Up_Motor_Raw_Velocity(value_z), min_up, max_up,-MOTOR_MAX_PWM,MOTOR_MAX_PWM);
 }
+#endif
 
-
-//this function is used to enable the use of compute_XXX_Motor_Speed 
+//this function is used to enable the use of compute_XXX_Motor_Velocity 
 void compute_Resize_Parameters(){
-   min_right = compute_Right_Motor_Raw_Speed(-100.0, -100.0);
-   max_right = compute_Right_Motor_Raw_Speed(100.0, -100.0);
-   min_left = compute_Left_Motor_Raw_Speed(-100.0, 100.0);
-   max_left = compute_Left_Motor_Raw_Speed(100.0, 100.0);   
-   min_up = compute_Up_Motor_Raw_Speed(-100.0);
-   max_up = compute_Up_Motor_Raw_Speed(100.0);   
+   min_right = compute_Right_Motor_Raw_Velocity(-100.0, -100.0);
+   max_right = compute_Right_Motor_Raw_Velocity(100.0, -100.0);
+   min_left = compute_Left_Motor_Raw_Velocity(-100.0, 100.0);
+   max_left = compute_Left_Motor_Raw_Velocity(100.0, 100.0);   
+#ifndef TEST_XY_ONLY
+   min_up = compute_Up_Motor_Raw_Velocity(-100.0);
+   max_up = compute_Up_Motor_Raw_Velocity(100.0);   
+#endif
 }
 
 
@@ -88,27 +103,30 @@ void setup() {
  //initialize Serial communication
  Serial.begin(9600);
  Serial.flush();
- //initializing pins used to control motors 
- pinMode(motor_right_forward, OUTPUT);
- pinMode(motor_right_backward, OUTPUT);
- pinMode(motor_left_forward, OUTPUT);
- pinMode(motor_left_backward, OUTPUT); 
- pinMode(motor_up_forward, OUTPUT);
- pinMode(motor_up_backward, OUTPUT);
+ //initializing pins used to control motors power
+ pinMode(RIGHT_MOTOR_FORWARD, OUTPUT);
+ pinMode(RIGHT_MOTOR_BACKWARD, OUTPUT);
+ pinMode(LEFT_MOTOR_FORWARD, OUTPUT);
+ pinMode(LEFT_MOTOR_BACKWARD, OUTPUT);
+//initializing pins used to control motors rotation clockwise or trigonometricwise
+ digitalWrite(RIGHT_MOTOR_ENABLE, LOW);
+ digitalWrite(LEFT_MOTOR_ENABLE, LOW);
+
+#ifndef TEST_XY_ONLY 
+ pinMode(UP_MOTOR_FORWARD, OUTPUT);
+ pinMode(UP_MOTOR_BACKWARD, OUTPUT);
+ digitalWrite(UP_MOTOR_ENABLE, LOW);
+#endif
  
- 
- digitalWrite(motor_right_enable, LOW);
- digitalWrite(motor_left_enable, LOW);
- digitalWrite(motor_up_enable, LOW);
  compute_Resize_Parameters();
 }
 
 void loop() {
   
-    //continuously controlling motors
-    analogWrite(motor_right_enable, motor_right_pwm);
-    analogWrite(motor_left_enable, motor_left_pwm);
-    analogWrite(motor_up_enable, motor_up_pwm);
+    //continuously controlling motors by applying the computed pwm
+    analogWrite(RIGHT_MOTOR_ENABLE, right_motor_pwm);
+    analogWrite(LEFT_MOTOR_ENABLE, left_motor_pwm);
+    analogWrite(UP_MOTOR_ENABLE, up_motor_pwm);
     delay(100);//control loop = 10Hz
 }
 
@@ -120,44 +138,52 @@ void serialEvent(){
     incomingByte_X= constrain(incomingByte_X, -100,100);
     incomingByte_Y = Serial.read();
     incomingByte_Y= constrain(incomingByte_Y, -100,100);
+#ifndef TEST_XY_ONLY 
     incomingByte_Z = Serial.read();
     incomingByte_Z= constrain(incomingByte_Z, -100,100);
-    
-    //computing new speed for wheels  
+#endif    
+    //computing new desired velocity for each motor
+    right_motor_velocity = compute_Right_Motor_Velocity(float(incomingByte_X), float(incomingByte_Y));
+    left_motor_velocity = compute_Left_Motor_Velocity(float(incomingByte_X), float(incomingByte_Y));
+#ifndef TEST_XY_ONLY     
+    up_motor_velocity = compute_Up_Motor_Velocity(float(incomingByte_Z));
+#endif
     //configuring right motor PWM
-    motor_right_pwm = byte(abs(motor_right_speed));
-    if(motor_right_speed >= 0){
-        digitalWrite(motor_right_forward, HIGH);
-        digitalWrite(motor_right_backward, LOW);
+    right_motor_pwm = byte(abs(right_motor_velocity));
+    if(right_motor_velocity >= 0){
+        digitalWrite(RIGHT_MOTOR_FORWARD, HIGH);
+        digitalWrite(RIGHT_MOTOR_BACKWARD, LOW);
     }
     else{
-        digitalWrite(motor_right_forward, LOW);
-        digitalWrite(motor_right_backward, HIGH);
+        digitalWrite(RIGHT_MOTOR_FORWARD, LOW);
+        digitalWrite(RIGHT_MOTOR_BACKWARD, HIGH);
     }
     //configuring left motor PWM
-    motor_left_pwm = byte(abs(motor_left_speed));  
-    if(motor_left_speed >= 0){
-        digitalWrite(motor_left_forward, HIGH);
-        digitalWrite(motor_left_backward, LOW);
+    left_motor_pwm = byte(abs(left_motor_velocity));  
+    if(left_motor_velocity >= 0){
+        digitalWrite(LEFT_MOTOR_FORWARD, HIGH);
+        digitalWrite(LEFT_MOTOR_BACKWARD, LOW);
     }
     else{
-        digitalWrite(motor_left_forward, LOW);
-        digitalWrite(motor_left_backward, HIGH);
+        digitalWrite(LEFT_MOTOR_FORWARD, LOW);
+        digitalWrite(LEFT_MOTOR_BACKWARD, HIGH);
     }
-    //configuring up motor PWM
-    motor_up_pwm = byte(abs(motor_up_speed));  
-    if(motor_up_speed >= 0){
-        digitalWrite(motor_up_forward, HIGH);
-        digitalWrite(motor_up_backward, LOW);
+#ifndef TEST_XY_ONLY     
+   //configuring up motor PWM
+    up_motor_pwm = byte(abs(up_motor_velocity));  
+    if(up_motor_velocity >= 0){
+        digitalWrite(UP_MOTOR_FORWARD, HIGH);
+        digitalWrite(UP_MOTOR_BACKWARD, LOW);
     }
     else{
-        digitalWrite(motor_up_forward, LOW);
-        digitalWrite(motor_up_backward, HIGH);
+        digitalWrite(UP_MOTOR_FORWARD, LOW);
+        digitalWrite(UP_MOTOR_BACKWARD, HIGH);
     }
+#endif
     //some debugging info
-    //Serial.write(motor_right_pwm);
-    //Serial.write(motor_left_pwm);
-    //Serial.write(motor_up_pwm);
+    //Serial.write(right_motor_pwm);
+    //Serial.write(left_motor_pwm);
+    //Serial.write(up_motor_pwm);
     Serial.flush();   
   }
 }
