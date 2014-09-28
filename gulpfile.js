ff8290dd6c04959b9a72f90b330c327cac2c5cba
Fabrice BENTZ <fabrice.bentz@free.fr>
2014-09-28 23:07:49 +0200

    Add clean task and add dependencies on package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
+'use strict';
 var gulp = require('gulp');
+var del = require('del');
 
-gulp.task('test', function() {
+var options = {
+  build: './build',
+  src: '.src'
+};
+
+gulp.task('clean', function(cb) {
+  del(options.build, cb);
+});
+
+gulp.task('test', ['clean'],function() {
   return;
-})
+});
