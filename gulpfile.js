74a58faf158899976a60cfc633c2ddd606a80792
Fabrice BENTZ <fabrice.bentz@free.fr>
2014-09-29 00:58:24 +0200

    Add mocha and initialize test for server

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
 'use strict';
 var gulp = require('gulp');
 var del = require('del');
+var path = require('path');
+var mocha = require('gulp-mocha');
 
 var options = {
-  build: './build',
-  src: './src'
+  folders: {
+    build: './build',
+    src: './src',
+    tests: './tests'
+  },
 };
 
 gulp.task('clean', function(cb) {
   del(options.build, cb);
 });
 
-gulp.task('test', ['clean'],function() {
+gulp.task('test:server', function() {
+  return gulp.src(path.join(options.folders.tests, 'server/**/*_test.js'), {
+    read: false
+  })
+  .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('test', ['clean'], function() {
   return;
 });
---
+++ tests/server/socket_test.js
@@ -0,0 +1,10 @@
+/* global describe, it*/
+'use strict';
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('First test', function() {
+  it('should return 2', function() {
+    expect(1).to.be.equal(1);
+  });
+});
