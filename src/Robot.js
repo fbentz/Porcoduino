6415b791af3521dcbb659cfac70f1e61cc402882
Fabrice BENTZ <fabrice.bentz@talkspirit.com>
2014-10-23 13:29:23 +0200

    Add robot class and implement test

--- src/Robot.js
+++ src/Robot.js
@@ -1,8 +1,14 @@
-function Robot() {
+function Robot(serialPort) {
+  this.serialPort = serialPort;
 }
 
-Robot.prototype.setPosition = function(buffer) {
-  this.buffer = buffer;
+Robot.prototype.setSpeed = function(alpha, amplitude) {
+  this.x = Math.round(Math.cos(parseInt(alpha, 10)) * parseInt(amplitude, 10));
+  this.y = Math.round(Math.sin(parseInt(alpha, 10)) * -1 * parseInt(amplitude, 10));
 }
+
+Robot.prototype.move = function() {
+  this.serialPort.write(new Buffer([this.x,this.y]));
+};
 
 module.exports = Robot;
--- tests/server/robot_test.js
+++ tests/server/robot_test.js
@@ -1,31 +1,55 @@
 var chai = require('chai');
 var expect = chai.expect;
 var Robot = require('../../src/Robot');
+var sinon = require('sinon');
 
 describe('Robot', function() {
 
   describe('#contructor', function() {
     it('should be instanciate', function() {
-      var robot = new Robot();
+      var robot = new Robot
       expect(robot).to.be.instanceof(Robot);
+    });
+    it('should be instanciate with an adapater', function() {
+      var robot = new Robot();
     });
   });
 
-  describe('#setPosition', function() {
+  describe('#setSpeed', function() {
+    it('should set speed with an amplitude and alpha', function() {
+      var robot = new Robot();
+      robot.setSpeed(2, 100);
+      expect(robot.x).to.be.equal(-42);
+      expect(robot.y).to.be.equal(-91);
+    });
+  });
 
+  describe('#move', function() {
+    var sandbox;
+    var fakeSerialPort;
     var robot;
 
     beforeEach(function() {
-      robot = new Robot();
+      sandbox = sinon.sandbox.create();
+      fakeSerialPort = {
+        write: sinon.spy()
+      };
+      robot = new Robot(fakeSerialPort);
+      robot.x = 41;
+      robot.y = 90;
     });
 
-    afterEach(function() {
-      delete robot;
-    })
+    it('should write x,y on serial port', function(done) {
+      robot.move();
+      sinon.assert.calledWithExactly(fakeSerialPort.write, sinon.match(function(value) {
+        return JSON.stringify(value.toJSON())=== JSON.stringify([robot.x, robot.y]);
+      }));
+      done();
+    });
 
-    it('should have an array buffer with 2 dimension', function() {
-      robot.setPosition(new Buffer([100,100]));
-      expect(robot.buffer).to.be.instanceof(Buffer);
+    afterEach(function(done) {
+      sandbox.restore();
+      done();
     });
   });
 
